c988407ca0eaa269060ac6a383cb8f6fb347112a
Tor Sterner <sternerT@gmail.com>
2014-04-13 12:58:53 +0200

    Big refactoring of js code to be more OO

--- Pong/Scripts/pong/regularPong.js
+++ Pong/Scripts/pong/regularPong.js
@@ -1,129 +1,249 @@
-﻿var canvasWidth = 600;
-var canvasHeight = 400;
-var tableWidth = 600;
-var tableHeight = 380;
+﻿FpsWriter = function () {
+    var currentFps = $("#fps");
+    var possibleFps = $("#possibleFps");
 
-var Game = new Object();
+    var prevTime = Date.now();
+    this.possibleTimeSum = 0.0;
+    var frames = 0;
 
-var canvas = document.getElementById("regularPongCanvas");
-var context = canvas.getContext("2d");
+    this.updateFps = function () {
+        var currentTime = Date.now();
+        ms = currentTime - prevTime;
+        frames++;
 
-var currentFps = $("#fps");
-var possibleFps = $("#possibleFps");
+        if (currentTime > prevTime + 1000) { // One second since last update
+            var fps = Math.round((frames * 1000) / (currentTime - prevTime));
 
-var prevTime = Date.now();
-var possibleTimeSum = 0.0;
-var frames = 0;
+            var timePerLogicUpdate = this.possibleTimeSum / frames;
+            var possible = 1.0 / timePerLogicUpdate;
+            currentFps.html("" + fps);
+            possibleFps.html("" + possible);
 
-var updateFps = function () {
-    var currentTime = Date.now();
-    ms = currentTime - prevTime;
-    frames++;
-
-    if (currentTime > prevTime + 1000) { // One second since last update
-        var fps = Math.round((frames * 1000) / (currentTime - prevTime));
-
-        var timePerLogicUpdate = possibleTimeSum / frames;
-        var possible = 1.0 / timePerLogicUpdate;
-        currentFps.html("" + fps);
-        possibleFps.html("" + possible);
-
-        prevTime = currentTime;
-        frames = 0;
-        possibleTimeSum = 0.0;
+            prevTime = currentTime;
+            frames = 0;
+            this.possibleTimeSum = 0.0;
+        }
     }
 }
 
-var clearCanvas = function () {
-    context.fillStyle = "#000";
-    context.lineWidth = 1;
-    context.strokeStyle = '#000';
-    context.clearRect(0, 0, 600, 400);
-}
+Wall = function () {
+    this.positionY = 0;
+    this.size = 10;
 
-var drawWalls = function () {
-    context.lineWidth = 10;
-    context.beginPath();
-    context.moveTo(0, 5);
-    context.lineTo(600, 5);
-    context.stroke();
-    context.beginPath();
-    context.moveTo(0, 395);
-    context.lineTo(600, 395);
-    context.stroke();
-    context.lineWidth = 1;
-}
-
-var drawBall = function (xPos, yPos) {
-    context.beginPath();
-    context.arc(xPos, yPos, 10, 0, 2 * Math.PI, false);
-    context.fill();
-}
-
-var drawPaddles = function (leftYPos, leftHalfSize, rightYPos, rightHalfSize) {
-    context.lineWidth = 5;
-    context.beginPath();
-    context.moveTo(3, leftYPos - leftHalfSize);
-    context.lineTo(3, leftYPos + leftHalfSize);
-    context.stroke();
-    context.beginPath();
-    context.moveTo(tableWidth - 3, rightYPos - rightHalfSize);
-    context.lineTo(tableWidth - 3, rightYPos + rightHalfSize);
-    context.stroke();
-
-}
-
-Game.update = function () {
-
-}
-
-Game.draw = function () {
-    clearCanvas();
-    drawWalls();
-    drawBall(40, 100);
-    drawPaddles(50, 20, 250, 20);
-}
-
-Game.fps = 90;
-
-Game.run = function () {
-    var start = Date.now();
-    Game.update();
-    Game.draw();
-    var time = Date.now() - start;
-    possibleTimeSum += time / 1000;
-
-    updateFps();
-};
-
-var keyDownHandler = function (event) {
-    switch (event.keyCode) {
-        // key code for up arrow
-        case 38:
-            console.log('up arrow key pressed!');
-            break;
-
-            // key code for down arrow
-        case 40:
-            console.log('down arrow key pressed!');
-            break;
+    this.draw = function (table) {
+        table.context.lineWidth = this.size;
+        table.context.beginPath();
+        table.context.moveTo(0, this.positionY);
+        table.context.lineTo(table.canvasWidth, this.positionY);
+        table.context.stroke();
     }
 }
 
-function keyUpHandler(event) {
-    switch (event.keyCode) {
-        // key code for up arrow
-        case 38:
-            console.log('up arrow key released!');
-            break;
+Ball = function () {
+    this.direction = [0, 0];
+    this.position = [0, 0];
+    this.speed = 1;
+    this.size = 20;
 
-            // key code for down arrow
-        case 40:
-            console.log('down arrow key released!');
-            break;
+    this.setStart = function (posX, posY) {
+        this.position = [posX, posY];
+        var angle = Math.random() * Math.PI * 2;
+        this.direction = [Math.cos(angle), Math.sin(angle)];
+    }
+    this.draw = function (table) {
+        table.context.beginPath();
+        table.context.arc(this.position[0], this.position[1], this.size, 0, 2 * Math.PI, false);
+        table.context.fill();
+    }
+
+    this.update = function (table, walls, paddles) {
+        this.position[0] += this.direction[0] * this.speed;
+        this.position[1] += this.direction[1] * this.speed;
+        if (this.position[1] < this.size + walls[0].size) {
+            this.position[1] = (this.size + walls[0].size) +
+                (this.size + walls[0].size - this.position[1]);
+            this.direction[1] *= -1;
+        } else if (this.position[1] > table.canvasHeight - walls[1].size - this.size) {
+            this.position[1] = (table.canvasHeight - walls[1].size - this.size) +
+                (table.canvasHeight - walls[1].size - this.size - this.position[1]);
+            this.direction[1] *= -1;
+        }
     }
 }
 
-Game._intervalId = setInterval(Game.run, 1000 / Game.fps);
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
+Table = function () {
+    this.canvasWidth = 0;
+    this.canvasHeight = 0;
+    this.context = null;
+
+    this.initTable = function () {
+        var canvasTag = $("#regularPongCanvas");
+        this.canvasWidth = canvasTag.width();
+        this.canvasHeight = canvasTag.height();
+        this.context = canvasTag[0].getContext("2d");
+    }
+
+    this.clearCanvas = function () {
+        this.context.fillStyle = "#000";
+        this.context.lineWidth = 1;
+        this.context.strokeStyle = '#000';
+        this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+    }
+
+    this.initTable();
+    this.clearCanvas();
+}
+
+Paddle = function (paddleName, isAi) {
+    this.name = paddleName;
+    this.direction = 0;
+    this.positionY = 200;
+    this.positionX = 200;
+    this.speed = 2;
+    this.halfLength = 10;
+    this.width = 5;
+    this.isAi = isAi;
+
+    this.toString = function () {
+        return
+            "Paddle: " + this.name +
+            ", speed = " + this.speed * this.direction +
+            ", positionY = " + this.positionY +
+            ", size = " + this.halfLength;
+    }
+
+    this.getSpeed = function () {
+        return this.speed * this.direction;
+    }
+
+    this.draw = function (table) {
+        table.context.lineWidth = this.width;
+        table.context.strokeStyle = '#000';
+        table.context.beginPath();
+        table.context.moveTo(this.positionX, this.positionY - this.halfLength);
+        table.context.lineTo(this.positionX, this.positionY + this.halfLength);
+        table.context.stroke();
+    }
+
+    this.update = function (table, walls) {
+        var currentY = this.positionY + this.direction * this.speed;
+        if (currentY - this.halfLength < walls[0].size) {
+            currentY = this.halfLength + walls[0].size;
+            if (this.isAi) {
+                this.direction *= -1;
+            }
+        } else if (currentY + this.halfLength > table.canvasHeight - walls[1].size) {
+            currentY = table.canvasHeight - this.halfLength - walls[1].size;
+            if (this.isAi) {
+                this.direction *= -1;
+            }
+        }
+        this.positionY = currentY;
+    }
+}
+
+Game = function (gameName) {
+    var table = null;
+    var player = null;
+    var ai = null;
+    var fpsWriter = null;
+    var walls = [null, null];
+    var ball = null;
+    var paddles = [null, null];
+    var fps = 100;
+    var isRunning = false;
+
+    this.initGame = function () {
+        table = new Table();
+
+        player = new Paddle("Player");
+        player.positionX = player.width / 2;
+        ai = new Paddle("AI", true);
+        ai.positionX = table.canvasWidth - ai.width / 2;
+        ai.direction = Math.random() < 0.5 ? -1 : 1; // Sets a beginning random direction for AI
+        paddles[0] = player;
+        paddles[1] = ai;
+        fpsWriter = new FpsWriter();
+        walls[0] = new Wall();
+        walls[0].positionY = walls[0].size / 2;
+        walls[1] = new Wall();
+        walls[1].positionY = table.canvasHeight - walls[1].size / 2;
+        ball = new Ball();
+        ball.setStart(table.canvasWidth / 2, table.canvasHeight / 2);
+
+        document.addEventListener("keydown", this.keyDownHandler, false);
+        document.addEventListener("keyup", this.keyUpHandler, false);
+    }
+
+    var update = function () {
+        player.update(table, walls);
+        ai.update(table, walls);
+        ball.update(table, walls, paddles);
+    }
+    var draw = function () {
+        table.clearCanvas();
+        walls[0].draw(table);
+        walls[1].draw(table);
+        ball.draw(table);
+        player.draw(table);
+        ai.draw(table);
+    }
+
+    var run = function () {
+        var start = Date.now();
+        update();
+        draw();
+        var time = Date.now() - start;
+        fpsWriter.possibleTimeSum += time / 1000;
+        fpsWriter.updateFps();
+    }
+
+    var startGame = function () {
+        this._intervalId = setInterval(run, 1000 / fps);
+        console.log("Game started!");
+        isRunning = true;
+    }
+
+    var stopGame = function () {
+        clearInterval(this._intervalId);
+        console.log("Game stopped!");
+        isRunning = false;
+    }
+
+    this.keyDownHandler = function (event) {
+        switch (event.keyCode) {
+            // key code for up arrow
+            case 38:
+                //console.log('up arrow key pressed!');
+                player.direction = -1;
+                break;
+                // key code for down arrow
+            case 40:
+                //console.log('down arrow key pressed!');
+                player.direction = 1;
+                break;
+        }
+    }
+
+    this.keyUpHandler = function (event) {
+        switch (event.keyCode) {
+            // key code for Enter key
+            case 13:
+                if (isRunning) {
+                    stopGame();
+                } else {
+                    startGame();
+                }
+                break;
+                // key code for up arrow
+            case 38:
+                // key code for down arrow
+            case 40:
+                player.direction = 0;
+                break;
+        }
+    }
+}
+
+var pongGame = new Game("Regular Pong");
+pongGame.initGame();
