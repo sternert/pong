5442d42e09243bdb19ebe54496da11b7c2a5775c
Tor Sterner <sternerT@gmail.com>
2014-04-19 19:56:32 +0200

    Added an external fps meter, replaced loop with smoother one.

--- Pong/Scripts/pong/regularPong.js
+++ Pong/Scripts/pong/regularPong.js
@@ -1,4 +1,16 @@
-﻿function Vec2 (x, y) {
+﻿
+window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (callback) {
+            return window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+function Vec2(x, y) {
     this.x = x;
     this.y = y;
     this.getLength = function () {
@@ -12,39 +24,6 @@
     this.getAngle = function () {
         return Math.acos( this.x / this.getLength());
     };
-}
-
-var v1 = new Vec2(4, 4);
-console.log(v1.getLength());
-v1.normalize();
-console.log(v1.getLength());
-
-FpsWriter = function () {
-    var currentFps = $("#fps");
-    var possibleFps = $("#possibleFps");
-
-    var prevTime = Date.now();
-    this.possibleTimeSum = 0.0;
-    var frames = 0;
-
-    this.updateFps = function () {
-        var currentTime = Date.now();
-        ms = currentTime - prevTime;
-        frames++;
-
-        if (currentTime > prevTime + 1000) { // One second since last update
-            var fps = Math.round((frames * 1000) / (currentTime - prevTime));
-
-            var timePerLogicUpdate = this.possibleTimeSum / frames;
-            var possible = 1.0 / timePerLogicUpdate;
-            currentFps.html("" + fps);
-            possibleFps.html("" + possible);
-
-            prevTime = currentTime;
-            frames = 0;
-            this.possibleTimeSum = 0.0;
-        }
-    }
 }
 
 Wall = function () {
@@ -63,7 +42,7 @@ Wall = function () {
 function Ball() {
     this.direction = new Vec2(1, 0);
     this.position = new Vec2(0, 0);
-    this.speed = 2;
+    this.speed = 4.0;
     this.velocity = new Vec2(this.speed * this.direction.x, this.speed * this.direction.y);
     this.size = 10;
     this.reachedEnd = 0;
@@ -76,7 +55,12 @@ function Ball() {
     this.setStart = function (posX, posY) {
         this.position.x = posX;
         this.position.y = posY;
-        var angle = Math.random() * Math.PI * 2;
+        var angle = Math.random(); // Distribute only in one radian, first quarter to the right, second to the right
+        if (angle > 0.5) { // second quarter modified to go opposite the first
+            angle += 0.5;
+        }
+        angle -= 0.25; // Should be distributed from the middle of the board'
+        angle *= Math.PI;
         this.direction.x = Math.cos(angle);
         this.direction.y = Math.sin(angle);
         this.direction.normalize();
@@ -108,30 +92,23 @@ function Ball() {
         // Collision against Paddles
         if (this.position.x < this.size + paddles[0].width) { // At Player edge
             var offset = paddles[0].positionY - this.position.y;
-            if (Math.abs(offset) < paddles[0].halfLength) {
+            if (Math.abs(offset) < paddles[0].halfLength + this.size) {
                 this.position.x = (this.size + paddles[0].width) +
                 (this.size + paddles[0].width - this.position.x);
                 this.direction.x *= -1;
-                var offsetValue = offset / paddles[0].halfLength;
-                var newAngle = this.direction.getAngle() + offsetValue;
-                this.direction.x = Math.cos(newAngle);
-                this.direction.y = Math.sin(newAngle);
+                this.direction.y += (paddles[0].speed * paddles[0].direction) / 5;
                 this.direction.normalize();
                 this.updateVelocity();
             }
         } else if (this.position.x > table.canvasWidth - this.size - paddles[1].width) { // At AI edge
             var offset = paddles[1].positionY - this.position.y;
-            if (Math.abs(offset) < paddles[1].halfLength) {
+            if (Math.abs(offset) < paddles[1].halfLength + this.size) {
                 if (this.position.y > paddles[1].positionY - paddles[1].halfLength) {
                     this.position.x = (table.canvasWidth - paddles[1].width - this.size) +
                         (table.canvasWidth - paddles[1].width - this.size - this.position.x);
                     this.direction.x *= -1;
-                    var offsetValue = offset / paddles[1].halfLength;
-                    var newAngle = this.direction.getAngle() + offsetValue;
-                    this.direction.x = Math.cos(newAngle);
-                    this.direction.y = Math.sin(newAngle);
+                    this.direction.y += (paddles[1].speed * paddles[1].direction) / 5;
                     this.direction.normalize();
-                    this.updateVelocity();
                     this.updateVelocity();
                 }
             }
@@ -182,9 +159,9 @@ Paddle = function (paddleName, isAi) {
     this.direction = 0;
     this.positionY = 200;
     this.positionX = 200;
-    this.speed = 1;
+    this.speed = 4.0;
     this.halfLength = 40;
-    this.width = 20;
+    this.width = 10;
     this.isAi = isAi;
 
     this.toString = function () {
@@ -229,12 +206,12 @@ Game = function (gameName) {
     var table = null;
     var player = null;
     var ai = null;
-    var fpsWriter = null;
     var walls = [null, null];
     var ball = null;
     var paddles = [null, null];
-    var fps = 200;
+    var fps = 60;
     var isRunning = false;
+    var speedUpdateCounter = 0;
 
     var initGame = function () {
         table = new Table();
@@ -242,13 +219,12 @@ Game = function (gameName) {
         player = new Paddle("Player");
         //player.positionX = player.width / 2;
         player.positionX = table.canvasWidth - player.width / 2;
-        ai = new Paddle("AI", true);
+        ai = new Paddle("AI");
         ai.positionX = ai.width / 2;
         //ai.positionX = table.canvasWidth - ai.width / 2;
-        ai.direction = Math.random() < 0.5 ? -1 : 1; // Sets a beginning random direction for AI
+        //ai.direction = Math.random() < 0.5 ? -1 : 1; // Sets a beginning random direction for AI
         paddles[1] = player;
         paddles[0] = ai;
-        fpsWriter = new FpsWriter();
         walls[0] = new Wall();
         walls[0].positionY = walls[0].size / 2;
         walls[1] = new Wall();
@@ -268,6 +244,9 @@ Game = function (gameName) {
         player.update(table, walls);
         ai.update(table, walls);
         ball.update(table, walls, paddles);
+
+        updateSpeedOfBall();
+        checkVictoryConditions();
     }
     var draw = function () {
         table.clearCanvas();
@@ -290,26 +269,35 @@ Game = function (gameName) {
         }
     }
 
-    var run = function () {
-        var start = Date.now();
-        update();
-        draw();
-        var time = Date.now() - start;
-        fpsWriter.possibleTimeSum += time / 1000;
-        fpsWriter.updateFps();
-
-        checkVictoryConditions();
+    var updateSpeedOfBall = function () {
+        if (speedUpdateCounter > fps * 5) {
+            speedUpdateCounter = 0;
+            ball.speed *= 1.1;
+            console.log("Ball speed increase! Current: " + ball.speed);
+        } else {
+            speedUpdateCounter++;
+        }
     }
 
+    var fpsmeter = new FPSMeter({ decimals: 0, graph: true, theme: 'transparent', left: '5px'});
+
     var startGame = function () {
-        this._intervalId = setInterval(run, 1000 / fps);
+        function frame() {
+            fpsmeter.tickStart();
+            update();
+            draw();
+            if (isRunning) {
+                requestAnimationFrame(frame);
+            }
+            fpsmeter.tick();
+        }
+
         console.log("Game started!");
         isRunning = true;
+        requestAnimationFrame(frame);
     }
 
     var stopGame = function () {
-        clearInterval(this._intervalId);
-        console.log("Game stopped!");
         isRunning = false;
     }
 
@@ -319,11 +307,13 @@ Game = function (gameName) {
             case 38:
                 //console.log('up arrow key pressed!');
                 player.direction = -1;
+                ai.direction = -1;
                 break;
                 // key code for down arrow
             case 40:
                 //console.log('down arrow key pressed!');
                 player.direction = 1;
+                ai.direction = 1;
                 break;
         }
     }
@@ -343,6 +333,7 @@ Game = function (gameName) {
                 // key code for down arrow
             case 40:
                 player.direction = 0;
+                ai.direction = 0;
                 break;
         }
     }
